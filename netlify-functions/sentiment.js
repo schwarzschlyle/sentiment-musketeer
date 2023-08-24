1a242c029617acc1a39f4fbe44ebd20adfa07551
Lyle Kenneth <lylekeng@gmail.com>
2023-08-24 12:58:29 +0800

    netlify functions dummy

--- netlify-functions/sentiment.js
+++ netlify-functions/sentiment.js
@@ -36,16 +36,59 @@
 //  });
 //}
 
+//
+//const spawn = require('child_process').spawn;
+//
+//exports.handler = async function (event, context) {
+//  try {
+//    const userInput = JSON.parse(event.body).text;
+//    const result = await performSentimentAnalysis(userInput);
+//    return {
+//      statusCode: 200,
+//      body: JSON.stringify({ sentiment: result }),
+//    };
+//  } catch (error) {
+//    return {
+//      statusCode: 500,
+//      body: JSON.stringify({ error: 'An error occurred' }),
+//    };
+//  }
+//};
+//
+//function performSentimentAnalysis(text) {
+//  return new Promise((resolve, reject) => {
+//    const process = spawn('python', ['app.py', 'analyze', text]);
+//
+//    let sentiment = '';
+//
+//    process.stdout.on('data', (data) => {
+//      sentiment += data.toString();
+//    });
+//
+//    process.on('close', (code) => {
+//      if (code === 0) {
+//        resolve(sentiment.trim());
+//      } else {
+//        reject(new Error('Sentiment analysis failed'));
+//      }
+//    });
+//  });
+}
 
-const spawn = require('child_process').spawn;
+
+
+
+
+
+const axios = require('axios');
 
 exports.handler = async function (event, context) {
   try {
     const userInput = JSON.parse(event.body).text;
-    const result = await performSentimentAnalysis(userInput);
+    const response = await performSentimentAnalysis(userInput);
     return {
       statusCode: 200,
-      body: JSON.stringify({ sentiment: result }),
+      body: JSON.stringify({ sentiment: response.data.sentiment }),
     };
   } catch (error) {
     return {
@@ -55,22 +98,9 @@ exports.handler = async function (event, context) {
   }
 };
 
-function performSentimentAnalysis(text) {
-  return new Promise((resolve, reject) => {
-    const process = spawn('python', ['app.py', 'analyze', text]);
-
-    let sentiment = '';
-
-    process.stdout.on('data', (data) => {
-      sentiment += data.toString();
-    });
-
-    process.on('close', (code) => {
-      if (code === 0) {
-        resolve(sentiment.trim());
-      } else {
-        reject(new Error('Sentiment analysis failed'));
-      }
-    });
-  });
+async function performSentimentAnalysis(text) {
+  const apiUrl = 'YOUR_FLASK_APP_URL'; // Replace with your Flask app's URL
+  const response = await axios.post(apiUrl, { text });
+  return response;
 }
+
